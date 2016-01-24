fbc58dc7d67db2c48725a3d39993877b3313907f
Lazar Travica <trav326@gmail.com>
2016-01-24 18:45:54 +0100

    Cleaned up git tests.

--- git_test.go
+++ git_test.go
@@ -11,79 +11,71 @@ const REPO_GIT2GO = "libgit2/git2go"
 const REPO_MUX = "gorilla/mux"
 const REPO_CONSUL = "hashicorp/consul"
 
-//func TestGetRepo(t *testing.T) {
-//	repo, err := GetRepo(REPO_ENVY)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//	defer repo.Free()
-//
-//	defer CleanTempDir()
-//}
-//
-//func TestRepoWalking(t *testing.T) {
-//	repo, err := GetRepo(REPO_ENVY)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//	defer repo.Free()
-//
-//	WalkCommits(repo, func(_, _ *git.Commit) bool {
-//		return true
-//	})
-//
-//	defer CleanTempDir()
-//}
-//
-//func TestGetDiff(t *testing.T) {
-//	repo, err := GetRepo(REPO_ENVY)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//	defer repo.Free()
-//
-//	WalkCommits(repo, func(previous, current *git.Commit) bool {
-//		diff, err := GetDiff(repo, previous, current)
-//		if err != nil {
-//			t.Error(err)
-//			return false
-//		}
-//		defer diff.Free()
-//
-//		return true
-//	})
-//
-//	defer CleanTempDir()
-//}
-//
-//func TestWalkHunks(t *testing.T) {
-//	repo, err := GetRepo(REPO_ENVY)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//	defer repo.Free()
-//
-//	WalkCommits(repo, func(previous, current *git.Commit) bool {
-//		diff, err := GetDiff(repo, previous, current)
-//		if err != nil {
-//			t.Error(err)
-//			return false
-//		}
-//		defer diff.Free()
-//
-//		WalkHunks(diff, func(file git.DiffDelta, hunk git.DiffHunk) {
-//		})
-//
-//		return true
-//	})
-//
-//	defer CleanTempDir()
-//}
-
-func TestListFiles(t *testing.T) {
-	repo, _ := GetRepo(REPO_ENVY)
-
+func TestGetRepo(t *testing.T) {
+	repo, err := GetRepo(REPO_ENVY)
+	if err != nil {
+		t.Error(err)
+	}
 	defer repo.Free()
 
-	ListFiles(getTempDir() + string(os.PathSeparator) + REPO_ENVY)
+	defer CleanTempDir()
+}
+
+func TestRepoWalking(t *testing.T) {
+	repo, err := GetRepo(REPO_ENVY)
+	if err != nil {
+		t.Error(err)
+	}
+	defer repo.Free()
+
+	WalkCommits(repo, func(_, _ *git.Commit) bool {
+		return true
+	})
+
+	defer CleanTempDir()
+}
+
+func TestGetDiff(t *testing.T) {
+	repo, err := GetRepo(REPO_ENVY)
+	if err != nil {
+		t.Error(err)
+	}
+	defer repo.Free()
+
+	WalkCommits(repo, func(previous, current *git.Commit) bool {
+		diff, err := GetDiff(repo, previous, current)
+		if err != nil {
+			t.Error(err)
+			return false
+		}
+		defer diff.Free()
+
+		return true
+	})
+
+	defer CleanTempDir()
+}
+
+func TestWalkHunks(t *testing.T) {
+	repo, err := GetRepo(REPO_ENVY)
+	if err != nil {
+		t.Error(err)
+	}
+	defer repo.Free()
+
+	WalkCommits(repo, func(previous, current *git.Commit) bool {
+		diff, err := GetDiff(repo, previous, current)
+		if err != nil {
+			t.Error(err)
+			return false
+		}
+		defer diff.Free()
+
+		WalkHunks(diff, func(file git.DiffDelta, hunk git.DiffHunk) {
+		})
+
+		return true
+	})
+
+	defer CleanTempDir()
 }
